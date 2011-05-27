bd84bfd2041b38bcdfcb2812be8cd50e4f26db92
Andrew Johnson <andrew@gityou.com>
2011-05-26 22:50:39 -0700

    added a basic document view to test algorithm

--- pixel.js
+++ pixel.js
@@ -30,3 +30,15 @@
 
 
 */
+
+
+function render_depixelized(args) {
+  var image = args.image;
+  var canvas = document.getElementById(args.canvas);
+  var ctx = canvas.getContext ('2d');
+  
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect (10, 10, 55, 50);
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect (30, 30, 55, 50);
+}
